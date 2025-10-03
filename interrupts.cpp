300d4b557249a0aa52a2e86f120cd4efa4947ffa
naima-mamun <146744959+naima-mamun@users.noreply.github.com>
2025-10-02 20:40:00 -0400

    Update interrupts.cpp

--- interrupts.cpp
+++ interrupts.cpp
@@ -61,7 +61,7 @@ int main(int argc, char** argv) {
             int dev = duration_intr;
             int io_time = delays.at(dev);
 
-            execution += std:: tp_string(clocl) + ", " + std::to_string(io_time) + ", end of IO" + std::to_string(dev) + ", interuppt\n";
+            execution += std:: to_string(clock) + ", " + std::to_string(io_time) + ", end of IO" + std::to_string(dev) + ", interuppt\n";
             clock += io_time;
         }
 
